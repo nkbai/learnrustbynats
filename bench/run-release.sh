11aa928a1cc58500befc97edb60e9a7f35840166
bai <nkbai@163.com>
2020-02-13 11:48:17 +0800

    客户端，服务端批量发送 数据 有问题，有一些内存泄漏

--- bench/run-release.sh
+++ bench/run-release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
- cargo run  --release  -- --urls 127.0.0.1:4222 --subject test --num-subs 10 --num-msgs 100000 --num-pubs 1
+ cargo run  --release  -- --urls 127.0.0.1:4222 --subject test --num-subs 10 --num-msgs 100000 --num-pubs 10
